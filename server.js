299d4602448f3c03a9b33c252d7dbc56e6e4b483
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-10-25 23:54:22 +0530

    updated cors

--- server.js
+++ server.js
@@ -25,11 +25,9 @@ const authRoute = require('./routes/auth');
 
 //middleware
 const corsOptions = {
-	origin: 'https://e-commerce-react-app-rouge.vercel.app/', // Replace with your React app's origin
+	origin: '*', // Replace with your React app's origin
 	credentials: true, // Enable credentials (cookies) in cross-origin requests
 };
-
-app.get('Origin');
 
 app.use(cors(corsOptions));
 
