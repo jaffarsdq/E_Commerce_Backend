09e5edb9fe69e651fcf8d2adf6dfc206d04f3b0e
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-10-25 23:59:29 +0530

    updated corsOptions

--- server.js
+++ server.js
@@ -24,10 +24,19 @@ const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 
 //middleware
+const allowedOrigins = ['https://e-commerce-react-app-rouge.vercel.app'];
+
 const corsOptions = {
-	origin: '*', // Replace with your React app's origin
-	credentials: true, // Enable credentials (cookies) in cross-origin requests
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow credentials (cookies)
 };
+
 
 app.use(cors(corsOptions));
 
